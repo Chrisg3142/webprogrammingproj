35adb63f147d6ce869e0627b78e2a88f2fe7c725
Chris <christophergutama123@gmail.com>
2024-10-07 21:55:14 -0400

    testing JavaScritp

--- giftcardsenter.js
+++ giftcardsenter.js
@@ -7,9 +7,9 @@ let code_entry = document.getElementById("messagebox");
 
 button.addEventListener("click", function() {
     if (code_entry.length < 9){
-        code_entry.innerText = "The code should be a 9 alpha numeric code";
+        code_entry.innerHTML = "The code should be a 9 alpha numeric code";
     } else {
-        code_entry.innerText = thank_you_message;
+        code_entry.innerHTML = thank_you_message;
     }
     console.log("the end")
 })
