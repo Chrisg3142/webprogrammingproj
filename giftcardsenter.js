23e7ea134bd7454d16d74aca0a0d16e0c11e763f
Chris <christophergutama123@gmail.com>
2024-10-07 23:03:01 -0400

    added trim for whitespace and cleaned up code a bit

--- giftcardsenter.js
+++ giftcardsenter.js
@@ -1,7 +1,7 @@
 const inputfield = document.getElementById('entered_code');
 
-document.getElementById('enterbutton').addEventListener('click', function() {
-    const code = inputfield.value;
+function Submitting_code() {
+    const code = inputfield.value.trim();
     // inputfield is just where the user entered the code but renamed so the value can be stored or else code.length wont work since its not a value in Javascript if we dont use the .value;
     if (code.length == 9) {
         inputfield.value = 'Thank you for your submission!'; 
@@ -11,7 +11,9 @@ document.getElementById('enterbutton').addEventListener('click', function() {
         inputfield.value = 'Enter a code!'
     }
     inputfield.setAttribute('readonly', true); 
-});
+}
+
+document.getElementById('enterbutton').addEventListener('click', Submitting_code);
 
 inputfield.addEventListener('click', function() {
     if (inputfield.readOnly) {
@@ -19,3 +21,4 @@ inputfield.addEventListener('click', function() {
         inputfield.value = ''; 
     }
 });
+
