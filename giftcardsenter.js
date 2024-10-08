6d37dd99238ca909a85ceef6f04b44ae2d5832bc
Chris <christophergutama123@gmail.com>
2024-10-07 21:45:15 -0400

    trying to fix message issue

--- giftcardsenter.js
+++ giftcardsenter.js
@@ -6,6 +6,11 @@ let button = document.getElementById("enterbutton");
 let code_entry = document.getElementById("messagebox");
 
 button.addEventListener("click", function() {
-    code_entry.innerText = thank_you_message;
+    if (code_entry.length < 9){
+        code_entry.innerText = "The code should be a 9 alpha numeric code";
+    } else {
+        code_entry.innerText = thank_you_message;
+    }
+    console.log("the end")
 })
 
