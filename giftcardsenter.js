e6c5278b9ce3bd341b343a3ef280fd003c45f679
Chris <christophergutama123@gmail.com>
2024-10-07 21:25:29 -0400

    adding jaav scsript for the gift card page

---
+++ giftcardsenter.js
@@ -0,0 +1,8 @@
+
+console.log("hgellow")
+const button = document.getElementById("enterbutton");
+let code_entry = document.getElementById("text")
+button.addEventListener("click", function(){
+    code_entry.innerText = "Thank you for submitting you code"
+})
+
