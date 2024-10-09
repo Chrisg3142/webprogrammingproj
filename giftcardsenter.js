dbbd434017fc3a2bc5680d58e16b56a1637db4f6
Chris <christophergutama123@gmail.com>
2024-10-08 22:14:28 -0400

    added function to check if the code entered contained letters

--- giftcardsenter.js
+++ giftcardsenter.js
@@ -1,10 +1,20 @@
 const inputfield = document.getElementById('entered_code');
 
+function checking_for_letter (code) {
+    const letters = /[a-zA-z]/;
+    return letters.test(code)
+}
+
 function Submitting_code() {
     const code = inputfield.value.trim();
     // inputfield is just where the user entered the code but renamed so the value can be stored or else code.length wont work since its not a value in Javascript if we dont use the .value;
     if (code.length == 9) {
-        inputfield.value = 'Thank you for your submission!'; 
+        if (checking_for_letter(code) == true){
+            inputfield.value = 'Thank you for your submission!'; 
+        }
+        else {
+            inputfield.value = 'Please enter a valid code!';
+        }
     } else if (code.length > 0) {
         inputfield.value = 'Please enter a valid code!';
     } else {
